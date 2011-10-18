d509269f33a86cdb7905519569e29f089ce2fa22
Steven Robbins <ste.robbins@gmail.com>
2011-10-18 13:15:22 +0100

    Initial commit

---
+++ src/Nancy.Serializers.Json.ServiceStack/Nancy.Serializers.Json.ServiceStack/ServiceStackBodyDeserializer.cs
@@ -0,0 +1,48 @@
+﻿namespace Nancy.Serializers.Json.ServiceStack
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+
+    using Nancy.ModelBinding;
+
+    using global::ServiceStack.Text;
+
+    public class ServiceStackBodyDeserializer : IBodyDeserializer
+    {
+        public bool CanDeserialize(string contentType)
+        {
+            return Helpers.IsJsonType(contentType);
+        }
+
+        public object Deserialize(string contentType, Stream bodyStream, BindingContext context)
+        {
+            var deserializedObject = JsonSerializer.DeserializeFromStream(context.DestinationType, bodyStream);
+
+            if (context.DestinationType.GetProperties(BindingFlags.Public | BindingFlags.Instance).Except(context.ValidModelProperties).Any())
+            {
+                return this.CreateObjectWithBlacklistExcluded(context, deserializedObject);
+            }
+
+            return deserializedObject;
+        }
+
+        private object CreateObjectWithBlacklistExcluded(BindingContext context, object deserializedObject)
+        {
+            var returnObject = Activator.CreateInstance(context.DestinationType);
+
+            foreach (var property in context.ValidModelProperties)
+            {
+                this.CopyPropertyValue(property, deserializedObject, returnObject);
+            }
+
+            return returnObject;
+        }
+
+        private void CopyPropertyValue(PropertyInfo property, object sourceObject, object destinationObject)
+        {
+            property.SetValue(destinationObject, property.GetValue(sourceObject, null), null);
+        }
+    }
+}
---
+++ src/Nancy.Serializers.Json.ServiceStack/Nancy.Serializers.Json.ServiceStack/ServiceStackJsonSerializer.cs
@@ -0,0 +1,20 @@
+﻿namespace Nancy.Serializers.Json.ServiceStack
+{
+    using System;
+    using System.IO;
+
+    using global::ServiceStack.Text;
+
+    public class ServiceStackJsonSerializer : ISerializer
+    {
+        public bool CanSerialize(string contentType)
+        {
+            return Helpers.IsJsonType(contentType);
+        }
+
+        public void Serialize<TModel>(string contentType, TModel model, Stream outputStream)
+        {
+            JsonSerializer.SerializeToStream(model, outputStream);
+        }
+    }
+}
