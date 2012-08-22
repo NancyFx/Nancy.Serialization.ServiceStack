6a5c11d7bfe96717fb0d48ed4151815996864cdc
Andreas Hakansson <andreas@selfinflicted.org>
2012-08-22 23:02:05 +0200

    Updated ServiceStack dependency

--- src/Nancy.Serialization.ServiceStack/Nancy.Serialization.ServiceStack/ServiceStackJsonSerializer.cs
+++ src/Nancy.Serialization.ServiceStack/Nancy.Serialization.ServiceStack/ServiceStackJsonSerializer.cs
@@ -1,17 +1,37 @@
 ﻿namespace Nancy.Serializers.Json.ServiceStack
 {
-    using System;
+    using System.Collections.Generic;
     using System.IO;
-
     using global::ServiceStack.Text;
 
     public class ServiceStackJsonSerializer : ISerializer
     {
+        /// <summary>
+        /// Whether the serializer can serialize the content type
+        /// </summary>
+        /// <param name="contentType">Content type to serialise</param>
+        /// <returns>True if supported, false otherwise</returns>
         public bool CanSerialize(string contentType)
         {
             return Helpers.IsJsonType(contentType);
         }
 
+        /// <summary>
+        /// Gets the list of extensions that the serializer can handle.
+        /// </summary>
+        /// <value>An <see cref="IEnumerable{T}"/> of extensions if any are available, otherwise an empty enumerable.</value>
+        public IEnumerable<string> Extensions
+        {
+            get { yield return "json"; }
+        }
+
+        /// <summary>
+        /// Serialize the given model with the given contentType
+        /// </summary>
+        /// <param name="contentType">Content type to serialize into</param>
+        /// <param name="model">Model to serialize</param>
+        /// <param name="outputStream">Output stream to serialize to</param>
+        /// <returns>Serialised object</returns>
         public void Serialize<TModel>(string contentType, TModel model, Stream outputStream)
         {
             JsonSerializer.SerializeToStream(model, outputStream);
